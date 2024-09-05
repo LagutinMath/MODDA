3068ef01277e7981f5053905f907ceaf9fa85e12
LagutinMath <lagutin.math@gmail.com>
2024-09-05 19:35:02 +0300

    set plot style

--- modda/MainWindow.py
+++ modda/MainWindow.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import (QMainWindow, QWidget, QVBoxLayout, QFileDialog)
-from PySide6.QtGui import QAction
+from PySide6.QtGui import QAction, QFont
 import pyqtgraph as pg
 from DepositionMeasurements import DepositionMeasurements
 from paths import meas_dir
@@ -19,11 +19,27 @@ class MainWindow(QMainWindow):
         self.central_widget.setLayout(self.layout)
 
         # Initialize plot widget and scatter (to be updated later)
-        self.plot_widget = pg.PlotWidget()
+        self.plot_widget = pg.PlotWidget(background='w')
         self.layout.addWidget(self.plot_widget)
 
+        # Set plot appearance
+        axis_font = QFont('Times New Roman', 16)
+
+        def styled_txt(txt):
+            return f"<span style='font-size: 16pt; font-family: Times New Roman; color: black;'>{txt}</span>"
+
+        self.plot_widget.getPlotItem().getAxis('bottom').setTextPen('black')
+        self.plot_widget.getPlotItem().getAxis('left').setTextPen('black')
+        self.plot_widget.getPlotItem().getAxis('bottom').setPen(pg.mkPen(color='black', width=2))
+        self.plot_widget.getPlotItem().getAxis('left').setPen(pg.mkPen(color='black', width=2))
+        self.plot_widget.getPlotItem().getAxis('bottom').setStyle(tickFont=axis_font)
+        self.plot_widget.getPlotItem().getAxis('left').setStyle(tickFont=axis_font)
+
+        self.plot_widget.getPlotItem().setLabel('left', styled_txt('Transmittance (abs)'))
+        self.plot_widget.getPlotItem().setLabel('bottom', styled_txt('Time (s)'))
+
         # Create scatter plot item but do not set data yet
-        self.scatter = pg.ScatterPlotItem(size=10, pen=pg.mkPen(None), brush=pg.mkBrush(255, 0, 0))
+        self.scatter = pg.ScatterPlotItem(size=5, pen=pg.mkPen(None), brush=pg.mkBrush(0, 0, 160))
         self.plot_widget.addItem(self.scatter)
 
         # Initial data plot
